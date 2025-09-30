b82315e2257e8ebf3b2c0eda4b61c4a9858a89de
Galina <baleva1990@gmail.com>
2025-09-30 22:59:20 +0200

    Add Home route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-// import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 import Home from "./pages/Home"
 import Navigation from "./pages/Navigation"
@@ -9,9 +9,14 @@ function App() {
 
   return (
     <>
+      <BrowserRouter>
         <Navigation />
-        <Home />
-
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
 
     </>
   )
