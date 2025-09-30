1d4911d00c442e83f5c5bf6331301cd678d02de2
Galina <baleva1990@gmail.com>
2025-09-30 23:01:50 +0200

    Add Util and Item components

---
+++ client/src/components/Units.tsx
@@ -0,0 +1,83 @@
+import Item from "./Item";
+
+
+interface Apartments {
+    [name: string]: Apartment;
+}
+
+interface Apartment {
+    image: string;
+    name: string;
+    guests: number;
+    numberOfBeds: number;
+    typeOfBed: [string, string?, string?];
+    rating: number;
+}
+
+
+const typeApartments: Apartments = {
+    cozyStudio: {
+        name: "Cozy Studio",
+        image: './public/IMG_0417s-1mbs-light-scaled.jpg',
+        guests: 2,
+        numberOfBeds: 1,
+        typeOfBed: ['double bed'],
+        rating: 4
+    },
+    charmingStudio: {
+        image: './public/IMG_0417s-1mbs-light-scaled.jpg',
+        name: "Charming Studio",
+        guests: 2,
+        numberOfBeds: 1,
+        typeOfBed: ['single bed'],
+        rating: 4
+    },
+    cozyOneBedroom: {
+        image: './public/IMG_0417s-1mbs-light-scaled.jpg',
+        name: "Cozy One Bedroom Apartment",
+        guests: 3,
+        numberOfBeds: 2,
+        typeOfBed: ['single bed', 'sofa'],
+        rating: 4
+    },
+    charmingOneBedroom: {
+        image: './public/IMG_0417s-1mbs-light-scaled.jpg',
+        name: "Charming Studio",
+        guests: 4,
+        numberOfBeds: 2,
+        typeOfBed: ['single bed', 'sofa'],
+        rating: 4
+    },
+    cozyTwoBedroom: {
+        image: './public/IMG_0417s-1mbs-light-scaled.jpg',
+        name: "Cozy One Bedroom Apartment",
+        guests: 5,
+        numberOfBeds: 3,
+        typeOfBed: ['single bed', 'double bed', 'sofa'],
+        rating: 4
+    },
+    charmingTowBedroom: {
+        image: './public/IMG_0417s-1mbs-light-scaled.jpg',
+        name: "Charming Studio",
+        guests: 6,
+        numberOfBeds: 3,
+        typeOfBed: ['single bed', 'double bed', 'sofa'],
+        rating: 4
+    },
+}
+
+const Units = () => {
+
+    return (
+        <section className="w-full py-8 px-10 flex flex-col gap-6">
+            <h2 className="text-center text-3xl text-orange-400 font-dancing">Available Apartments</h2>
+            <div className="flex justify-center gap-1 flex-wrap">
+                {Object.values(typeApartments).map((apartment: Apartment) => <Item key={apartment.name} {...apartment} />)}
+            </div>
+
+
+        </section>
+    )
+}
+
+export default Units;
